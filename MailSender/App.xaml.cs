511c6e1007f00fa2de4fce832841f628797a7c6a
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-06-14 21:25:52 +0300

    Добавьте файлы проекта.

---
+++ MailSender/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MailSender
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
