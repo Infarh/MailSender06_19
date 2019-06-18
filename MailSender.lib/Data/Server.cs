5ece51931d1f4b58cc76ab8a8938b87fa136acaf
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-06-18 20:39:06 +0300

    Подключение простейших источников данных

--- MailSender.lib/Data/Server.cs
+++ MailSender.lib/Data/Server.cs
@@ -2,6 +2,7 @@
 {
     public class Server
     {
+        public string Name { get; set; }
         public string Address { get; set; }
         public int Port { get; set; } = 25;
         public bool UseSSL { get; set; } = true;
