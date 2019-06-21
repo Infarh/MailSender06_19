7afdd5d8ffb061f75385ed1ea2173e3ac9cc228f
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-06-21 22:09:42 +0300

    Реализация MVVM для основного проекта + реализация первого сервиса извлечения данных о получателях почты по средствам Linq2SQL

---
+++ MailSender/View/RecipientInfoEditor.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace MailSender.View
+{
+    /// <summary>Представление редактора данных получателя почты</summary>
+    public partial class RecipientInfoEditor
+    {
+        public RecipientInfoEditor() => InitializeComponent();
+    }
+}
