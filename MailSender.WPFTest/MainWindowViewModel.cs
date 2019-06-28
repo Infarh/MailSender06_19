8a6d93e036fab21712f6923d99cde92d8d194612
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-06-28 18:56:09 +0300

    Удаление хлама

--- MailSender.WPFTest/MainWindowViewModel.cs
+++ MailSender.WPFTest/MainWindowViewModel.cs
@@ -11,54 +11,6 @@ namespace MailSender.WPFTest
     /// <summary>Пример собственной подели представления</summary>
     class MainWindowViewModel : ViewModel
     {
-        /// <summary>Поле для хранения данных заголовка</summary>
-        private string _Title = "Test Application";
-
-        /// <summary>Заголовок</summary>
-        public string Title
-        {
-            get => _Title;
-            set => Set(ref _Title, value);
-        }
-
-        /// <summary>Поле для хранения данных положения</summary>
-        private double _Position;
-
-        /// <summary>Положение</summary>
-        public double Position
-        {
-            get => _Position;
-            set => Set(ref _Position, value);
-        }
-
-        /// <summary>Поле для хранения данных угла</summary>
-        private double _Angle;
-
-        /// <summary>Угол</summary>
-        public double Angle
-        {
-            get => _Angle;
-            set => Set(ref _Angle, value);
-        }
-
-        /// <summary>Поле для хранени яданных времени</summary>
-        private DateTime _Time = DateTime.Now;
-
-        /// <summary>Время</summary>
-        public DateTime Time
-        {
-            get => _Time;
-            private set => Set(ref _Time, value);
-        }
-
-        /// <summary>Таймер обновления времени</summary>
-        private readonly DispatcherTimer _Timer = new DispatcherTimer
-        {
-            Interval = TimeSpan.FromMilliseconds(1),
-            IsEnabled = true
-        };
-
-        /// <summary>Новая модель-представления главного окна</summary>
-        public MainWindowViewModel() => _Timer.Tick += (s, e) => Time = DateTime.Now;
+        
     }
 }
