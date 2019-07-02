14bfb0eea15bcbdc0ea96c8fd3877a73443892e7
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-07-02 18:52:23 +0300

    Подготовка к 6 уроку - удаление хлама

--- MailSender.ConsoleTest/Program.cs
+++ MailSender.ConsoleTest/Program.cs
@@ -8,27 +8,6 @@ namespace MailSender.ConsoleTest
     {
         private static void Main(string[] args)
         {
-            //ThreadTest.Start();
-
-            //ThreadSynchronizationTest.Start();
-
-            //ThreadPoolTest.Start();
-
-            #region Загадка!
-            var threads = new Thread[10];
-
-            for (var i = 0; i < threads.Length; i++)
-            {
-                threads[i] = new Thread(() => Console.WriteLine($"Сообщение №{i}"));
-
-                //var i0 = i;
-                //threads[i] = new Thread(() => Console.WriteLine($"Сообщение №{i0}"));
-            }
-
-            for (var i = 0; i < threads.Length; i++)
-                threads[i].Start(); 
-            #endregion
-
             Console.ReadLine();
         }
     }
