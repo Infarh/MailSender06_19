0404518baf0bb9e8d194e3d45554fe1fae23ee4c
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-07-05 18:35:43 +0300

    Шзменение в интерфейсе сервисов доступа к данным

--- MailSender.lib/Services/IDataService.cs
+++ MailSender.lib/Services/IDataService.cs
@@ -30,27 +30,32 @@ namespace MailSender.lib.Services
 
         /// <summary>Добавить новый элемент в сервис</summary>
         /// <returns>Возвращает идентификатор добавленной сущности</returns>
-        void Add(T item);
+        int Add(T item);
 
         /// <summary>Асинхронно добавить новый элемент в сервис</summary>
         /// <param name="item">Добавляемый элемент</param>
-        /// <returns>Задача по добавлению нового элемента в сервис</returns>
-        Task AddAsync(T item);
+        /// <returns>Задача возращает идентификатор добавленного элемента</returns>
+        Task<int> AddAsync(T item);
 
         /// <summary>Редактировать элемент в сервисе</summary>
-        void Edit(T item);
+        /// <param name="id">Идентификатор редактируемого элемента</param>
+        /// <param name="item">Редактируемый элемент</param>
+        T Edit(int id, T item);
 
         /// <summary>Асинхронно редактировать элемент в сервисе</summary>
+        /// <param name="id">Идентификатор редактируемого элемента</param>
         /// <param name="item">Редактируемый элемент</param>
         /// <returns>Задача внесения изменений в данные сервиса</returns>
-        Task EditAsync(T item);
+        Task<T> EditAsync(int id, T item);
 
         /// <summary>Удалить</summary>
-        void Delete(T item);
+        /// <param name="id">Идентификатор удаляемого элемента</param>
+        /// <returns>Истина, если элемент удалён успешно</returns>
+        bool Delete(int id);
 
         /// <summary>Асинхронно удалить элемент из данных сервиса</summary>
-        /// <param name="item">Удаляемый элемент</param>
-        /// <returns>Задача удаления элемента из сервиса</returns>
-        Task DeleteAsync(T item);
+        /// <param name="id">Идентификатор удаляемого элемента</param>
+        /// <returns>Задача, возврадающая истину, если элемент успешно удалён</returns>
+        Task<bool> DeleteAsync(int id);
     }
 }
