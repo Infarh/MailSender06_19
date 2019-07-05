dd3911ae1b88f8c3830c47f22e1bb9f1b5bbca3c
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-07-05 18:23:06 +0300

    Чистка кода (удаление хлама, комментарии)

--- MailSender.ConsoleTest/Program.cs
+++ MailSender.ConsoleTest/Program.cs
@@ -8,44 +8,10 @@ namespace MailSender.ConsoleTest
 {
     internal class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
-            //TaskTest.Start();
-            //TPLTest.Start();
-            //AsyncAwaitTest.Start();
-
-            //await Task.Run(() => Console.WriteLine("Параллельное действие в Main"));
-
-            Func<string, int> function = LongDataFunction;
-
-            var data = "Hello World";
-
-            //var result = function(data);
-            //result = function.Invoke(data);
-
-            //var async_result = function.BeginInvoke(data, r =>
-            //{
-            //    var func_result = function.EndInvoke(r);
-            //    Console.WriteLine("Результат выполнения {0}", func_result);
-            //}, null);
-
-            var delegate_task = Task.Factory.FromAsync(
-                function.BeginInvoke,
-                function.EndInvoke,
-                data, null);
-
-            var f_result = await delegate_task;
-
-            Console.WriteLine("Основной поток завершился");
+           
             Console.ReadLine();
-        }
-
-        private static int LongDataFunction(string Data)
-        {
-            Console.WriteLine("Вычисляю длина строки: {0}", Data);
-            Thread.Sleep(100);
-            Console.WriteLine("Lлина строки {0} = {1}", Data, Data.Length);
-            return Data.Length;
         }
     }
 }
--- MailSender.lib/Services/IDataService.cs
+++ MailSender.lib/Services/IDataService.cs
@@ -6,31 +6,51 @@ using System.Threading.Tasks;
 
 namespace MailSender.lib.Services
 {
+    /// <summary>Сервис доступа к данным</summary>
+    /// <typeparam name="T">Тип данных хранимого элемента</typeparam>
     public interface IDataService<T>
     {
-        /// <summary>Извлечь</summary>
+        /// <summary>Извлечь все элементы</summary>
+        /// <returns>Перечисление всех элементов, хранимых в сервисе</returns>
         IEnumerable<T> GetAll();
 
+        /// <summary>Асинхронно извлечь все элементы</summary>
+        /// <returns>Задача, возвращающая перечисление всех элементов, хранимых в сервисе</returns>
         Task<IEnumerable<T>> GetAllAsync();
 
+        /// <summary>Получить элемент по заданному идентификатору</summary>
+        /// <param name="id">Идентификатор элемента</param>
+        /// <returns>Элемент с заданным идентификатором, либо null</returns>
         T GetById(int id);
 
+        /// <summary>Асинхронного получить элемент по заданному идентификатору</summary>
+        /// <param name="id">Идентификатор элемента</param>
+        /// <returns>Задача асинхронного получения элемента с заданным идентификатором</returns>
         Task<T> GetByIdAsync(int id);
 
-        /// <summary>Создать (зарегистрировать)</summary>
+        /// <summary>Добавить новый элемент в сервис</summary>
         /// <returns>Возвращает идентификатор добавленной сущности</returns>
         void Add(T item);
 
+        /// <summary>Асинхронно добавить новый элемент в сервис</summary>
+        /// <param name="item">Добавляемый элемент</param>
+        /// <returns>Задача по добавлению нового элемента в сервис</returns>
         Task AddAsync(T item);
 
-        /// <summary>Обновить</summary>
+        /// <summary>Редактировать элемент в сервисе</summary>
         void Edit(T item);
 
+        /// <summary>Асинхронно редактировать элемент в сервисе</summary>
+        /// <param name="item">Редактируемый элемент</param>
+        /// <returns>Задача внесения изменений в данные сервиса</returns>
         Task EditAsync(T item);
 
         /// <summary>Удалить</summary>
         void Delete(T item);
 
+        /// <summary>Асинхронно удалить элемент из данных сервиса</summary>
+        /// <param name="item">Удаляемый элемент</param>
+        /// <returns>Задача удаления элемента из сервиса</returns>
         Task DeleteAsync(T item);
     }
 }
