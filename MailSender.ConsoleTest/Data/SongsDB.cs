bb790e08d6315b11a7752e6badc33f2393dc5311
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-07-05 21:13:42 +0300

    Пример простейшей БД

---
+++ MailSender.ConsoleTest/Data/SongsDB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ConsoleTest.Data
+{
+    public class SongsDB : DbContext
+    {
+        public SongsDB() : this("Name=SongsDB") { }
+
+        public SongsDB(string ConnectionString) : base(ConnectionString) // Либо имя строки подключения из app.config, либо саму строку подключения
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+
+    [Table("Songs")]
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual Artist Artist { get; set; } //  virtual - используется не по прямому назначению!  virtual говорит EF, что свойство является "навигационным".
+    }
+
+    //[Table("Artist")]
+    public class Artist
+    {
+        //[Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
--- MailSender.ConsoleTest/Program.cs
+++ MailSender.ConsoleTest/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MailSender.ConsoleTest.Data;
 
 namespace MailSender.ConsoleTest
 {
@@ -10,7 +11,47 @@ namespace MailSender.ConsoleTest
     {
         private static void Main(string[] args)
         {
-           
+            using (var db = new Data.SongsDB())
+            {
+                Console.WriteLine("Songs count - {0}", db.Songs.Count());
+            }
+
+            using (var db = new SongsDB())
+            {
+                if (!db.Songs.Any())
+                {
+                    var songs = new Song[100];
+                    for (var i = 0; i < songs.Length; i++)
+                        songs[i] = new Song
+                        {
+                            Name = $"Песня {i + 1}",
+                            Artist = new Artist { Name = $"Исполнитель {i}" }
+                        };
+                    db.Songs.AddRange(songs);
+
+                    db.SaveChanges();
+                }
+            }
+
+            //using (var db = new Data.SongsDB())
+            //{
+            //    foreach (var song in db.Songs.ToArray())
+            //    {
+            //        Console.WriteLine("Song {0} - artist {1}", song.Name, song.Artist.Name);
+            //    }
+            //}
+
+            using (var db = new SongsDB())
+            {
+                db.Database.Log = str => Console.WriteLine(str); // Указыаем контексту БД куда писать лог
+
+                var song = db.Songs.FirstOrDefault(s => s.Artist.Name == "Исполнитель 9");
+                if (song != null)
+                    db.Songs.Remove(song);
+
+                db.SaveChanges();
+            }
+
             Console.ReadLine();
         }
     }
